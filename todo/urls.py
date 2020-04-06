1f3307663e2c16c42cf56aa83b7727948b3bc806
casey <nagasawa311@gmail.com>
2020-04-06 10:11:28 +0900

    initial commit

---
+++ todo/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import TodoList,TodoDetail,TodoCreate,TodoDelete
+
+urlpatterns = [
+    path("list/",TodoList.as_view(),name="list"),
+    path("detail/<int:pk>",TodoDetail.as_view(),name="detail"),
+    path("create/",TodoCreate.as_view(),name="create"),
+    path("delete/<int:pk>",TodoDelete.as_view(),name="delete")
+
+    # pathにnameをつけることで、この経路に名前をつけることができる。reverse_lazyとかで使う
+]
---
+++ todo/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView,CreateView,DeleteView
+from .models import TodoModel
+from django.urls import reverse_lazy
+
+# Create your views here.
+class TodoList(ListView):
+    template_name="list.html"
+    model=TodoModel
+
+class TodoDetail(DetailView):
+    template_name="detail.html"
+    model=TodoModel
+
+class TodoCreate(CreateView):
+    template_name="create.html"
+    model=TodoModel
+    fields=('title','memo','priority','duedate')
+    success_url=reverse_lazy("list")
+
+class TodoDelete(DeleteView):
+    template_name="delete.html"
+    model=TodoModel
+    success_url=reverse_lazy("list")
+
+# classの時はreverse_lazyを使う。methodの時はreverse　を使うらしい。
